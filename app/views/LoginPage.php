b3928f9f3fde700c8083fbe0720485f0c134ded0
antoine <antoinetu2003@gmail.com>
2026-03-26 15:15:43 +0100

    add image broccoli-image.png

--- app/views/LoginPage.php
+++ app/views/LoginPage.php
@@ -10,12 +10,13 @@
 
 <body class="login-page">
     <main class="login-wrapper">
+        <img src="/assets/broccoli-image.png" alt="Broccoli" style="width: 200px; margin-bottom: 40px;">
         <section class="login-card">
             <h1>Connexion</h1>
 
             <form class="login-form" method="post" action="#">
                 <label for="email">Adresse email</label>
-                <input type="email" id="email" name="email" placeholder="exemple@mail.com" required>
+                <input type="email" id="email" name="email" placeholder="broccoli@exemple.com" required>
 
                 <label for="password">Mot de passe</label>
                 <input type="password" id="password" name="password" placeholder="********" required>
@@ -25,6 +26,7 @@
 
             <a class="back-link" href="/signup">S'inscrire</a>
         </section>
+        <img src="/assets/broccoli-image.png" alt="Broccoli" style="width: 200px; margin-bottom: 40px;">
     </main>
 </body>
 
